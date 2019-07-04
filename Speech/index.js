50940db2e1a99c9db950adb2cfe45a390dbc36ec
Avani <avanibhatnagar07@gmail.com>
2019-07-04 11:51:33 -0700

    weather

---
+++ Speech/index.js
@@ -0,0 +1,16 @@
+let request = require('request');
+const argv = require('yargs').argv;
+
+let apiKey = 'b4a1442b7de6e009718834d9270f25de';
+let city = argv.c || 'portland';
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+
+request(url, function (err, response, body) {
+  if(err){
+    console.log('error:', error);
+  } else {
+    let weather = JSON.parse(body)
+    let message = `It's ${weather.main.temp} degrees in ${weather.name}!`;
+    console.log(message);
+  }
+});
